4454f3ccced68b29fc49890e7a258709ba932944
Cameron Lakenen <lakenen@box.com>
2014-07-21 16:51:22 -0700

    Fix parse issues with multiple quote-types and escaped quotes

--- test/test.js
+++ test/test.js
@@ -7,10 +7,23 @@ describe('spawn-args', function(){
   })
 
   it('should parse some arguments', function() {
-    var arr = parse('--host hello.com --port 80 --title "Apple Pie With Spaces" --num 20')    
+    var arr = parse('--host hello.com --port 80 --title "Apple Pie With Spaces" --num 20')
 
     arr.length.should.equal(8);
     arr[5].should.equal('"Apple Pie With Spaces"');
   })
 
+  it('should parse multiple quotes properly', function() {
+    var arr = parse('--data \'{"url": "http://example.com"}\'')
+
+    arr.length.should.equal(2);
+    arr[1].should.equal('\'{"url": "http://example.com"}\'');
+  })
+
+  it('should parse escaped quotes properly', function() {
+    var arr = parse('--data "{\\"url\\": \\"http://example.com\\"}"')
+
+    arr.length.should.equal(2);
+    arr[1].should.equal('"{\\"url\\": \\"http://example.com\\"}"');
+  })
 })
